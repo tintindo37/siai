46f87e836240565af0ed35fd335ff02e8d494d10
tintindo37 <tintin.do.37@gmail.com>
2024-09-11 11:46:50 +0200

    działa

--- 10.09.2024/forms.php
+++ 10.09.2024/forms.php
@@ -5,19 +5,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $imienazw =  $_POST["imienazw"];
     $number = $_POST["number"];
     $email = $_POST["email"];
-    $urodziny = $_POST["urodziny"]
-    if(empty($gender) && empty($imienazw) && empty($number) && empty($email) && empty($urodziny)){
-        echo "Twoje imię i nazwisko to $imienazw";
-        echo "Twoja płeć to $gender";
-        echo "Twój numer to $number";
-        echo "Twój email to $email";
-        echo "Twoje urodziny to $urodziny";
+    $urodziny = $_POST["urodziny"];
+    if(!empty($gender) && !empty($imienazw) && !empty($number) && !empty($email) && !empty($urodziny)){
+        echo "Twoje imię i nazwisko to $imienazw <br>";
+        echo "Twoja płeć to $gender <br>";
+        echo "Twój numer to $number <br>";
+        echo "Twój email to $email <br>";
+        echo "Twoje urodziny to $urodziny <br>";
     }
     else{
-        echo "Nie podano dannych";
+        echo "Nie podano dannych <br>";
     }
 }
 else{
-    echo "Nie jest post";
+    echo "Nie jest post <br>";
 }
 ?>
