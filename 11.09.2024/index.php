55b10a136059b2243f32f1e5d95a204501266e30
tintindo37 <tintin.do.37@gmail.com>
2024-09-18 12:18:07 +0200

    skonczone

--- 11.09.2024/index.php
+++ 11.09.2024/index.php
@@ -7,17 +7,17 @@
     <title>Kalkulator</title>
 </head>
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER ["PHP_SELF"]); ?>" method="post">
-        <input type="number" name="num01" placeholder="numer jeden">
-        <select name="wybierz">
-            <option value="+">+</option>
-            <option value="-">-</option> 
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
-        <input type="number" name="num02" placeholder="numer dwa">
-        <button>Kalkuluj</button>
-    </form>
+        <form action="<?php echo htmlspecialchars($_SERVER ["PHP_SELF"]); ?>" method="post">
+            <input type="number" name="num01" placeholder="numer jeden">
+            <select name="wybierz">
+                <option value="+">+</option>
+                <option value="-">-</option> 
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input type="number" name="num02" placeholder="numer dwa">
+            <button>Kalkuluj</button>
+        </form>
 <?php
     $num01 = $_POST["num01"];
     $num02 = $_POST["num02"];
@@ -26,19 +26,19 @@
     echo "liczba 2 to " .$num02. "<br>";
     if ($wybierz ==="+") {
         $obliczenie = $num01+$num02;
-        echo "wynik sumy to"  .$obliczenie ;
+        echo "wynik sumy to "  .$obliczenie ;
     }
     if ($wybierz === "-") {
         $obliczenie = $num01-$num02;
-        echo "wynik to" .$obliczenie. "<br>";
+        echo "wynik to " .$obliczenie. "<br>";
     }
     if ($wybierz ==="*") {
         $obliczenie = $num01*$num02;
-        echo "wynik to" . $obliczenie;
+        echo "wynik to " . $obliczenie;
     }
     if ($wybierz ==="/"){
         $obliczenie = $num01/$num02;
-        echo "wynik to" .$obliczenie;
+        echo "wynik to " .$obliczenie;
     }
     ?>   
 </body>
---
+++ 18.09.2024/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html><!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Hai Dang Do Hoang">
+    <link rel="stylesheet" href="styles.css">
+    <title>Kalkulator</title>
+</head>
+<body>
+    <div>
+        <form action="<?php echo htmlspecialchars($_SERVER ["PHP_SELF"]); ?>" method="post">
+            <input type="number" name="num01" placeholder="Podaj pierwszą liczbę">
+            <select name="wybierz">
+                <option value="+">+</option>
+                <option value="-">-</option> 
+                <option value="*">*</option>
+                <option value="/">/</option>
+                <option value="%">%</option>
+            </select>
+            <input type="number" name="num02" placeholder="Podaj drugą liczbę">
+            <br>
+            <button>Kalkuluj</button>
+        </form>
+    </div>
+<?php
+    if(isset($_POST["num01"]) && isset($_POST["num01"]) && isset($_POST["num02"]) && isset( $_POST["wybierz"])) {
+        $num01 = $_POST["num01"];
+        $num02 = $_POST["num02"];
+        $wybierz = $_POST["wybierz"];  
+        echo "liczba pierwsza to " .$num01. "<br>";
+        echo "liczba druga to " .$num02. "<br>";
+        if ($wybierz ==="+") {
+            $obliczenie = $num01+$num02;
+            echo "wynik sumy to "  .$obliczenie ;
+        }
+        if ($wybierz === "-") {
+            $obliczenie = $num01-$num02;
+            echo "wynik to " .$obliczenie. "<br>";
+        }
+        if ($wybierz ==="*") {
+            $obliczenie = $num01*$num02;
+            echo "wynik to " . $obliczenie;
+        }
+        if ($wybierz ==="/"){
+            $obliczenie = $num01/$num02;
+            echo "wynik to " .$obliczenie;
+        }
+        if ($wybierz ==="%"){
+            $obliczenie = $num01%$num02;
+            echo "wynik to " .$obliczenie;
+        }
+    }
+    ?>   
+</body>
+</html>
